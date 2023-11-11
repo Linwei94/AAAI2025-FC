7f7dfa0eab6a75cf2440b4675341285ea71b4728
Linwei Tao <dylan.tao94@gmail.com>
2023-11-11 07:26:16 +0000

    update

--- help.sh
+++ help.sh
@@ -1,5 +1,6 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --model resnet34 --name resnet34_ce& 
-CUDA_VISIBLE_DEVICES=2 python train.py --model resnet50 --name resnet50_ce&
-CUDA_VISIBLE_DEVICES=3 python train.py --model resnet110 --name resnet110_ce&
-CUDA_VISIBLE_DEVICES=1 python train.py --model wide_resnet --name wide_resnet_ce &
-CUDA_VISIBLE_DEVICES=2 python train.py --model densenet121 --name densenet121_ce
+CUDA_VISIBLE_DEVICES=1 python train.py --model resnet18 --loss mmce --dataset cifar100 --name c100_resnet18_mmce& 
+CUDA_VISIBLE_DEVICES=0 python train.py --model resnet34 --loss mmce --dataset cifar100 --name c100_resnet34_mmce&
+CUDA_VISIBLE_DEVICES=2 python train.py --model resnet50 --loss mmce --dataset cifar100 --name c100_resnet50_mmce&
+CUDA_VISIBLE_DEVICES=3 python train.py --model resnet110 --loss mmce --dataset cifar100 --name c100_resnet110_mmce&
+CUDA_VISIBLE_DEVICES=0 python train.py --model wide_resnet --loss mmce --dataset cifar100 --name c100_wide_resnet_mmce &
+CUDA_VISIBLE_DEVICES=2 python train.py --model densenet121 --loss mmce --dataset cifar100 --name c100_densenet121_mmce&
